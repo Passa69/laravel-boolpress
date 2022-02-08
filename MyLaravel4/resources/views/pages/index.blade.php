21abb2a19c43fa47960427a6ed31c66571254896
tommaso <passarotto13@gmail.com>
2022-02-08 17:28:17 +0100

    correzione per visualizzazione tag

--- MyLaravel4/resources/views/pages/index.blade.php
+++ MyLaravel4/resources/views/pages/index.blade.php
@@ -5,7 +5,9 @@
         <h1>{{ $post -> title }}</h1>
         <h4>Author: {{ $post -> author }}</h4>
         <h4>Category: {{ $post -> category -> title }}</h4>
-        <h4>Tag: {{ $post -> tags }}</h4>
+        @foreach ($post -> tags as $tag)
+            <h4>Tag: {{$tag -> name}}</h4>
+        @endforeach
         <span>Rating: {{ $post -> rating }}</span>
     </div>
 
